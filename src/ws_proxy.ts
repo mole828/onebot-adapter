54497d924d28193c63a3120df23190f0eb930a57
mole828 <mrx800@163.com>
2024-04-26 17:54:12 +0800

    add: 在ws_proxy添加错误捕获

--- src/ws_proxy.ts
+++ src/ws_proxy.ts
@@ -7,16 +7,24 @@ export class WebSocketProxy {
     private ws_in: WebSocket 
     private ws_out: WebSocket 
     public midware: Midware = (d)=>d;
+    private _midware: Midware = (d)=>{
+        try {
+            return this.midware(d);
+        } catch (e){
+            console.log({error:e, data:d});
+        }
+        return d;
+    };
     constructor(ws_in: WebSocket, ws_out: WebSocket) {
         this.ws_in = ws_in;
         this.ws_out = ws_out;
         if(ws_out.readyState===WebSocket.OPEN){
-            ws_in.onmessage = e => ws_out.send(this.midware(e.data));
+            ws_in.onmessage = e => ws_out.send(this._midware(e.data));
         } else {
             const buffer: Array<WebSocket.Data> = [];
-            ws_in.onmessage = e => buffer.push(this.midware(e.data));
+            ws_in.onmessage = e => buffer.push(this._midware(e.data));
             ws_out.onopen = ()=>{
-                ws_in.onmessage = e => ws_out.send(this.midware(e.data));
+                ws_in.onmessage = e => ws_out.send(this._midware(e.data));
                 buffer.forEach(data=>ws_out.send(data));
             };
         }
